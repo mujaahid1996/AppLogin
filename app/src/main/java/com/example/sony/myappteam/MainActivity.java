690da49b39c8b7d67f2cc43faf2ba7fe2be93712
Muj <mujaahid1996@gmail.com>
2017-05-23 01:11:26 +0700

    Awal Commit

---
+++ app/src/main/java/com/example/sony/myappteam/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sony.myappteam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //inisialisasi var
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //deklarasi var
+
+    }
+}
