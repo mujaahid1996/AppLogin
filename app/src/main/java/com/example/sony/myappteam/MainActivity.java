0a57995d1183797765fc01e370bd6cee39c32a43
Muj <mujaahid1996@gmail.com>
2017-05-23 01:59:42 +0700

    Commit MainActivity

--- app/src/main/java/com/example/sony/myappteam/MainActivity.java
+++ app/src/main/java/com/example/sony/myappteam/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.sony.myappteam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //inisialisasi var
+    EditText username, passwd;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //deklarasi var
+        username = (EditText) findViewById(R.id.usernameEt);
+        passwd = (EditText) findViewById(R.id.passEt);
+        login = (Button) findViewById(R.id.loginBtn);
 
     }
+
+    public void post(View v) {
+        String id, pass, url;
+        id = username.getText().toString();
+        pass = passwd.getText().toString();
+
+        //alamat IP untuk parsing JSON
+        url = "http://androtes1.000webhostapp.com/_getLoginUser.php?username=" + id + "&password=" + pass;
+
+        new Getter(this, url, 0);
+    }
+
+
 }
